cfe35b553c92446df4af1fb16abb31c4795a50d7
Patrick Dantas <patrick.dantas@inventsoftware.com.br>
2018-07-11 18:19:05 -0300

    .

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -38,37 +38,12 @@ class Perceptron:
                     # faz o ajuste dos pesos para cada elemento da amostra\n"
                     for j in range(self.n_atributos + 1):
                         self.pesos[j] = self.pesos[j] + self.taxa_aprendizado * erro_aux * self.amostras[i][j]
-                    erro = True # o erro ainda existe\n"    
-                n_epocas += 1 # incrementa o número de épocas\n"    
-                # critério de parada\n",
-                if not erro or n_epocas > self.epocas:
-                    break
-
-#não sei se a identação tá certa
-"\t\twhile True:\n",
-    "\n",
-    "\t\t\terro = False # erro inicialmente inexiste\n",
-    "\n",
-    "\t\t\tfor i in range(self.n_amostras):\n",
-    "\t\t\t\tu = 0\n",
-    "\t\t\t\tfor j in range(self.n_atributos + 1):\n",
-    "\t\t\t\t\tu += self.pesos[j] * self.amostras[i][j]\n",
-    "\t\t\t\ty = self.sinal(u) # obtém a saída da rede\n",
-    "\n",
-    "\t\t\t\t# verifica se a saída da rede é diferente da saída desejada\n",
-    "\t\t\t\tif y != self.saidas[i]:\n",
-    "\t\t\t\t\t# calcula o erro\n",
-    "\t\t\t\t\terro_aux = self.saidas[i] - y\n",
-    "\t\t\t\t\t# faz o ajuste dos pesos para cada elemento da amostra\n",
-    "\t\t\t\t\tfor j in range(self.n_atributos + 1):\n",
-    "\t\t\t\t\t\tself.pesos[j] = self.pesos[j] + self.taxa_aprendizado * erro_aux * self.amostras[i][j]\n",
-    "\t\t\t\t\terro = True # o erro ainda existe\n",
-    "\n",
-    "\t\t\tn_epocas += 1 # incrementa o número de épocas\n",
-    "\n",
-    "\t\t\t# critério de parada\n",
-    "\t\t\tif not erro or n_epocas > self.epocas:\n",
-    "\t\t\t\tbreak\n",
+                    erro = True # o erro ainda existe\n"  
+                    
+            n_epocas += 1 # incrementa o número de épocas\n"    
+            # critério de parada\n",
+            if not erro or n_epocas > self.epocas:
+                break
 
     def teste(self, amostra):
         amostra.insert(0, -1)
@@ -87,3 +62,42 @@ class Perceptron:
         if u >= 0:
             return 1
         return -1
+
+amostras = [[0, 0], [0, 1], [1, 0], [1, 1]]
+saidas = [0, 1, 1, 1]
+rede = Perceptron(amostras, saidas)
+rede.treinar()
+rede.teste([1, 1])
+
+# outro exemplo
+amostras = [[0.1, 0.4, 0.7], [0.3, 0.7, 0.2],
+            [0.6, 0.9, 0.8], [0.5, 0.7, 0.1]]
+saidas = [1, -1, -1, 1]
+rede = Perceptron(amostras, saidas)
+rede.treinar()
+rede.teste([0.5, 0.7, 0.1])
+
+# azul ou vermelho?
+amostras = [[0.72, 0.82], [0.91, -0.69],
+            [0.46, 0.80],   [0.03, 0.93],
+            [0.12, 0.25],   [0.96, 0.47],
+            [0.8, -0.75],   [0.46, 0.98],
+            [0.66, 0.24],   [0.72, -0.15],
+            [0.35, 0.01],   [-0.16, 0.84],
+            [-0.04, 0.68],  [-0.11, 0.1],
+            [0.31, -0.96],   [0.0, -0.26],
+            [-0.43, -0.65],  [0.57, -0.97],
+            [-0.47, -0.03],  [-0.72, -0.64],
+            [-0.57, 0.15],   [-0.25, -0.43],
+            [0.47, -0.88],   [-0.12, -0.9],
+            [-0.58, 0.62],   [-0.48, 0.05],
+            [-0.79, -0.92],  [-0.42, -0.09],
+            [-0.76, 0.65],   [-0.77, -0.76]]
+
+saidas = [-1,-1,-1,-1,-1,-1, -1,-1,
+         -1,-1,-1,-1,-1,1,1,1,1,
+         1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+rede = Perceptron(amostras, saidas)
+rede.treinar()
+rede.teste([0.46, 0.80])
